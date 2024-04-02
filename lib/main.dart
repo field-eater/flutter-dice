8c8bd94edb5d41d65015e8ec6ed11599eb9b490b
field-eater <atunphil@gmail.com>
2024-04-02 15:18:33 +0800

    Flutter Dice

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:basics/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(colors: [
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 98, 16, 7)
+        ]),
+      ),
+    ),
+  );
+}
